fc537ea3014b37605b38a6514c2d8bdb20ea9243
TwicePie <kknurarief@gmail.com>
2025-01-27 12:43:23 +0700

    Enhance AOS initialization with custom duration, easing, and once settings

--- src/components/aoswrapper.js
+++ src/components/aoswrapper.js
@@ -6,7 +6,11 @@ import "aos/dist/aos.css";
 
 const AOSWrapper = ({ children }) => {
   useEffect(() => {
-    AOS.init(); // Initialize AOS when component mounts
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+    });
   }, []);
 
   return <>{children}</>;
